be1c97b473ae6ed6ab17047d6cb4ac04ba070b1a
Gordon Martin <gordonhughmartin@gmail.com>
2022-02-28 20:36:57 +0000

    stuff i forgot to push

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,21 @@ provider "aws" {
   region  = var.aws_region
 }
 
+# this data source can be used to get the access to the effective Account ID, User ID, and ARN in which Terraform is authorized 
+data "aws_caller_identity" "current" {}
+
+locals {
+  root_dir   = "${path.module}/.."
+  account_id = data.aws_caller_identity.current.account_id
+
+  default_memory_size = 128
+  default_timeout     = 10
+  lambda_functions = {
+    checkerboard = {
+      function_name = "api-lambda"
+      memory_size   = local.default_memory_size
+      timeout       = local.default_timeout
+      build_args    = "--build-arg log_level=${var.log_level}"
+    }
+  }
+}
