8218994224d4d135efb600e85545f8c1ad43e52a
Gordon Martin <gordonhughmartin@gmail.com>
2022-03-04 19:24:27 +0000

    further fannying around

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,11 +25,82 @@ locals {
   default_memory_size = 128
   default_timeout     = 10
   lambda_functions = {
-    checkerboard = {
+    apilambda = {
       function_name = "api-lambda"
       memory_size   = local.default_memory_size
       timeout       = local.default_timeout
       build_args    = "--build-arg log_level=${var.log_level}"
     }
   }
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "${var.service_name}-${local.lambda_functions["apilambda"].function_name}-${var.stage}"
+
+  image_uri    = "${aws_ecr_repository.lambda_repository.repository_url}@${data.aws_ecr_image.lambda_image.id}"
+  package_type = "Image"
+
+  timeout     = local.lambda_functions["checkerboard"].timeout
+  memory_size = local.lambda_functions["checkerboard"].memory_size
+  role        = aws_iam_role.lambda_role.arn
+}
+
+resource "aws_ecr_repository" "lambda_repository" {
+  name = "${var.service_name}-${local.lambda_functions["apilambda"].function_name}-${var.stage}"
+}
+
+resource "null_resource" "lambda_ecr_image_builder" {
+  triggers = {
+    docker_file     = filesha256("${local.root_dir}/Dockerfile")
+    cargo_file      = filesha256("${local.root_dir}/Cargo.toml")
+    cargo_lock_file = filesha256("${local.root_dir}/Cargo.lock")
+    src_dir         = sha256(join("", [for f in fileset("${local.root_dir}/src", "**") : filesha256("${local.root_dir}/src/${f}")]))
+  }
+
+  provisioner "local-exec" {
+    working_dir = local.root_dir
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<-EOT
+      aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
+      docker image build -t ${aws_ecr_repository.lambda_repository.repository_url}:latest ${local.lambda_functions["apilambda"].build_args} .
+      docker push ${aws_ecr_repository.lambda_repository.repository_url}:latest
+    EOT
+  }
+}
+
+data "aws_ecr_image" "lambda_image" {
+  depends_on = [
+    null_resource.lambda_ecr_image_builder
+  ]
+
+  repository_name = "${var.service_name}-${local.lambda_functions["apilambda"].function_name}-${var.stage}"
+  image_tag       = "latest"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.api_lambda.function_name}"
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.service_name}-${local.lambda_functions["apilambda"].function_name}-iam-role-${var.aws_region}-${var.stage}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = ""
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "basic_lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,23 @@
 variable "aws_region" {
   default = "eu-west-1"
 }
+
+variable "service_name" {
+  type = string
+  default = "podmax_service"
+}
+
+variable "stage" {
+  type = string
+  default = "dev"
+}
+
+variable "log_retention_in_days" {
+  type    = number
+  default = 30
+}
+
+variable "log_level" {
+  type    = string
+  default = "info"
+}
